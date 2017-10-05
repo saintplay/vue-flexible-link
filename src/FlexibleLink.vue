e9f1c102bfb20bec0c6884c59d5d93bd6d28869d
Diego Jara <saintplay96@gmail.com>
2017-10-05 15:09:56 -0500

    Add The Component

---
+++ src/FlexibleLink.vue
@@ -0,0 +1,41 @@
+<template > 
+  <a v-if="native" :class="customClass" @click="openLinkNatively">
+    <slot></slot>
+  </a>
+  <a v-else :class="customClass" :href="href" :target="target">
+    <slot></slot>
+  </a>
+</template> 
+
+<script>
+export default {
+  name: 'flexible-button',
+  props: {
+    native: {
+      type: Boolean,
+      default: true,
+    },
+    customClass: {
+      type: String,
+      default: null,
+    },
+    href: {
+      type: String,
+      default: '/',
+    },
+    target: {
+      type: String,
+      default: '_self',
+    },
+  },
+  methods: {
+    openLinkNatively() {
+      if (this.native) {
+        /* eslint-disable global-require */
+        require('electron').shell.openExternal(this.href);
+      }
+    },
+  },
+};
+
+</script>
